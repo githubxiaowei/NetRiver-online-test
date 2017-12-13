beb5871e162343b9fc742150e0b3683ad7e7fc1c
99250 <99250@SOFTLAB.LOCAL>
2017-12-13 12:23:35 +0800

    add report

--- ipv4_transfer.cpp
+++ ipv4_transfer.cpp
@@ -1,8 +1,6 @@
 #include "sysInclude.h"
 #include <vector>
-using std::vector;
-#include <iostream>
-using std::cout;
+
 // system support
 extern void fwd_LocalRcv(char *pBuffer, int length);
 
@@ -12,14 +10,14 @@ extern void fwd_DiscardPkt(char *pBuffer, int type);
 
 extern unsigned int getIpv4Address( );
 
-// implemented by students
+/* implemented by students
 typedef struct stud_route_msg
 {
 	unsigned int dest;
 	unsigned int masklen;
 	unsigned int nexthop;
 };
-
+*/
 struct routeTableItem
 {
 	unsigned int destIP;
@@ -27,92 +25,99 @@ struct routeTableItem
 	unsigned int masklen;
 	unsigned int nexthop;
 };
-vector<routeTableItem> m_table;
+//线性路由表
+vector<routeTableItem> routeTable;
 
-void stud_Route_Init()
+void stud_Route_Init()			
 {
-	m_table.clear();
+	routeTable.clear();
 }
 
 void stud_route_add(stud_route_msg *proute)
 {
-	routeTableItem newTableItem;
-	newTableItem.masklen = ntohl(proute->masklen);
-	newTableItem.mask = (1<<31)>>(ntohl(proute->masklen)-1);
-	newTableItem.destIP = ntohl(proute->dest)&newTableItem.mask;
-	newTableItem.nexthop = ntohl(proute->nexthop);
-	m_table.push_back(newTableItem);
+	routeTableItem item;
+	item.masklen = ntohl(proute->masklen);
+	item.mask = (1<<31)>>(ntohl(proute->masklen)-1);
+	item.destIP = ntohl(proute->dest)&item.mask;
+	item.nexthop = ntohl(proute->nexthop);
+	routeTable.push_back(item);
+}
+//计算校验和
+unsigned short _checksum(char *pBuffer)
+{
+    int sum = 0;
+    for(int i = 0; i < 10; ++i)
+    {
+        if(i != 5)
+        {
+            sum += ((unsigned short*)pBuffer)[i];
+        }
+    }
+    while(sum > 0xffff)
+    {
+        sum = (sum & 0xffff) + (sum >> 16); 
+    }
+    return (unsigned short)(0xffff - sum);
 }
 
 int stud_fwd_deal(char *pBuffer, int length)
 {
-	
-	int IHL = pBuffer[0] & 0xf; 
-	
+	//提取头部信息
+	int IHL = pBuffer[0] & 0xf; 	
 	int TTL = (int)pBuffer[8]; 
 	int headerChecksum = ntohl(*(unsigned short*)(pBuffer+10));
 	int destIP = ntohl(*(unsigned int*)(pBuffer+16));
-	
-	
+
+	//是否过期
+	if(TTL <= 0)
+	{
+		fwd_DiscardPkt(pBuffer, STUD_FORWARD_TEST_TTLERROR);
+		return 1;
+	}
+
+	//如果目标地址为本机
 	if(destIP == getIpv4Address())
 	{
 		fwd_LocalRcv(pBuffer, length);
 		return 0;
 	}
 	
-	
-	
-	if(TTL <= 0)
-	{
-		fwd_DiscardPkt(pBuffer, STUD_FORWARD_TEST_TTLERROR);
-		return 1;
-	}	
-	
-	
-	
-	
 	bool isMatch = false;
 	unsigned int longestMatchLen = 0;
 	int bestMatch = 0;
 	
-	for(int i = 0; i < m_table.size(); i ++)
+	//匹配最佳的地址
+	for(int i = 0; i < routeTable.size(); ++i)
 	{
-		if(m_table[i].masklen > longestMatchLen && m_table[i].destIP == (destIP & m_table[i].mask))
+		if(routeTable[i].masklen > longestMatchLen && routeTable[i].destIP == (destIP & routeTable[i].mask))
 		{
 			bestMatch = i;
 			isMatch = true;
-			longestMatchLen = m_table[i].masklen;
-			//cout << "find one" << endl;
+			longestMatchLen = routeTable[i].masklen;
 		}
 	}
 	
+	//匹配成功，更新TTL和校验和并转发
 	if(isMatch)
 	{
-		char *buffer = new char[length];  
-        memcpy(buffer,pBuffer,length);  
-        buffer[8]--; //TTL - 1
-		int sum = 0;  
-        unsigned short int localCheckSum = 0;
-        for(int j = 0; j < 2 * IHL; j ++)
-        {
-            if (j == 5)
-				continue;
-            sum = sum + (buffer[j*2]<<8) + (buffer[j*2+1]);
-        }
+		char *buffer = new char[length]; 
 
-		while((unsigned(sum) >> 16) != 0)
-			sum = unsigned(sum) >> 16 + sum & 0xffff;
+        memcpy(buffer,pBuffer,length);
 
-           	localCheckSum = htons(0xffff - (unsigned short int)sum);
+        --buffer[8];//TTL
+
+        unsigned short localCheckSum = _checksum(buffer);
+       
 		memcpy(buffer+10, &localCheckSum, sizeof(unsigned short));
 		
-		fwd_SendtoLower(buffer, length, m_table[bestMatch].nexthop);
+		fwd_SendtoLower(buffer, length, routeTable[bestMatch].nexthop);
 		return 0;
 	}
+	//没有找到目标地址，直接丢弃
 	else
 	{
 		fwd_DiscardPkt(pBuffer, STUD_FORWARD_TEST_NOROUTE);
 		return 1;
 	}
-	return 1;
+
 }
